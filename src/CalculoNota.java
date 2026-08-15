f9a25a902e229a5085cd0512611a80ebda07d242
Drey012 <drey01214@gmail.com>
2026-08-14 21:35:45 -0300

    Sistema básico de avaliação da professora Sirley simulado em Java

---
+++ src/CalculoNota.java
@@ -0,0 +1,15 @@
+public class CalculoNota {
+    public double calcularMedia1(double p1, double list, double p2){
+        return 0.35*p1 + 0.15*list + 0.5*p2;
+    }
+
+    public double calcularMedia2(double p1, double list, double p2, double p3){
+        if(p1<p2){
+            return 0.35*p3 + 0.15*list + 0.5*p2;
+        }
+        else {
+            return 0.35*p1 + 0.15*list + 0.5*p3;
+        }
+    }
+}
+
