88d3e9e50c01bd741aa40db5cdb8770a1f9fc4f2
Drey012 <drey01214@gmail.com>
2026-08-19 17:32:18 -0300

    Lógica de exame final aplicada para ALP

--- src/CalculoNota.java
+++ src/CalculoNota.java
@@ -11,5 +11,18 @@ public class CalculoNota {
             return 0.35*p1 + 0.15*list + 0.5*p3;
         }
     }
+
+    public double calcularMedia1OS(double p1, double p2){
+        return (p1 + p2)/2;
+    }
+
+    public double calcularMedia2OS(double p1, double p2, double p3){
+        if(p1<p2){
+            return (p3+p2)/2;
+        }
+        else {
+            return (p1+p3)/2;
+        }
+    }
 }
 
--- src/MenuGrafico.java
+++ src/MenuGrafico.java
@@ -35,7 +35,7 @@ public class MenuGrafico {
                 double mediaFinal = aritmetica.calcularMedia1ALP(p1, list, p2);
 
                 // 3. Define o status do aluno (Aprovado / Reprovado)
-                String status = (mediaFinal >= 6.0) ? "APROVADO" : "REPROVADO";
+                String status = (mediaFinal >= 6.0) ? "APROVADO" : "NECESSÁRIO P3";
 
                 // 4. Exibe o resultado final formatado com 2 casas decimais
                 String mensagemResultado = String.format(
@@ -49,9 +49,63 @@ public class MenuGrafico {
                 );
 
                 JOptionPane.showMessageDialog(null, mensagemResultado, "Resultado Final", JOptionPane.INFORMATION_MESSAGE);
+
+                if(mediaFinal < 6){
+                    double p3 = pedirNota("Digite a nota da Prova 3 (P3):");
+                    if (p3 == -1) return;
+
+                    double mediaFinal2 = aritmetica.calcularMedia2ALP(p1,list, p2, p3);
+                    status = (mediaFinal2 >= 6.0) ? "APROVADO" : (mediaFinal2 < 4) ? "REPROVADO, INELEGÍVEL PARA EXAME FINAL" : "NECESSÁRIO EXAME FINAL";
+
+                     mensagemResultado = String.format(
+                            "Média final com a P3: %.2f\n" +
+                                    "Situação: %s",
+                            mediaFinal2, status
+                     );
+
+                    JOptionPane.showMessageDialog(null, mensagemResultado, "Resultado Final", JOptionPane.INFORMATION_MESSAGE);
+
+                    if(mediaFinal2<6 && mediaFinal2>=4){
+                        double exame = pedirNota("Digite a nota do Exame Final:");
+                        if (exame == -1) return;
+
+                        status = (exame < 6) ? "REPROVADO" : "APROVADO";
+
+                        mensagemResultado = String.format(
+                                "Resultado do Exame Final: %.2f\n" +
+                                        "Situação: %s",
+                                exame, status
+                        );
+
+                        JOptionPane.showMessageDialog(null, mensagemResultado, "Resultado Final", JOptionPane.INFORMATION_MESSAGE);
+                    }
+                }
             }
             case 1 -> {
+                // 1. Pede as duas notas em sequência
+                double p1 = pedirNota("Digite a nota da Prova 1 (P1):");
+                if (p1 == -1) return; // Cancela se o usuário fechar a janela
 
+                double p2 = pedirNota("Digite a nota da Prova 2 (P2):");
+                if (p2 == -1) return;
+
+                // 2. Calcula a média usando a sua função
+                double mediaFinal = aritmetica.calcularMedia1OS(p1, p2);
+
+                //3. Define o status do aluno como (APROVADO/REPROVADO)
+                String status = (mediaFinal >= 6.0) ? "APROVADO" : "REPROVADO";
+
+                // 4. Exibe o resultado final formatado com 2 casas decimais
+                String mensagemResultado = String.format(
+                        "Resumo de Notas:\n" +
+                                "- P1: %.2f\n" +
+                                "- P2: %.2f\n\n" +
+                                "Média Final: %.2f\n" +
+                                "Situação: %s",
+                        p1, p2, mediaFinal, status
+                );
+
+                JOptionPane.showMessageDialog(null, mensagemResultado, "Resultado Final", JOptionPane.INFORMATION_MESSAGE);
             }
             case 2, -1 -> JOptionPane.showMessageDialog(null, "Sistema encerrado.");
         }
